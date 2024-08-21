4ca84eea0eab271d41c4618df1d044708e8a2844
Gabriel Henrique Vaz <58632934+ghvaz@users.noreply.github.com>
2024-08-21 18:48:54 -0300

    Create situacaoCarro.js

---
+++ src/controllers/situacaoCarro.js
@@ -0,0 +1,51 @@
+const carro = require("./controllers/modelos.js")
+let db=[]
+let proxId = 1
+const situacaoCarro = {
+    model: (body, id = nextID++) => {
+        if(body.carro_id != "" && body.carro_id != undefined){
+                return {id, carro: body.carro_id}
+            }
+            return undefined
+    },
+    
+    store: (body) => {
+        let novo = situacaoCarro.model(body)
+        if(novo){ 
+            db.push(novo)
+            return 201;
+        }else{
+            return 400;
+        }
+    },
+    showAll: () => { return db },
+
+    show: (ql) => {
+        let buscar = db.find(el => el.id == ql)
+        if(buscar){ return 200
+        }else{
+            return 418;
+        }
+    },
+
+    update: (qlID, body) => {
+        let bscID = db.findIndex(el => el.id == qlID)
+        let novoBody = situacaoCarro.model(body, parseInt(qlID))
+        if(novoBody && bscID != (-1)){
+            db[bscID] = novoBody
+            return 201;
+        }else{
+            return 400;
+        }
+    },
+
+    destroy: (qlID)=>{
+        let explodir = db.findIndex(el => el.id == qlID)
+        if(explodir != (-1)){
+            db.splice(explodir, 1)
+            return 200
+        }else{
+            return 400
+        }
+    },
+}
