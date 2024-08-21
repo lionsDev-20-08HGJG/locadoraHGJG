1316bb56551d2961b65fa7fca7710888d330c169
Gabriel Henrique Vaz <58632934+ghvaz@users.noreply.github.com>
2024-08-21 18:50:00 -0300

    Update tipoCarro.js

--- src/controllers/tipoCarro.js
+++ src/controllers/tipoCarro.js
@@ -1,6 +1,6 @@
 let db = []
 let nextId = 0
-const tipoCaroro = {
+const tipoCarro = {
     model:(body, id = nextId)=>{
         if(body.nome != "" && body.nome != undefined){
             return {id, nome: body.nome}
